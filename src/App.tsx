21efd00949977b335b14ab63e8a825bc713c6fa9
Nadish Sathsara <nadishsathsara2003@gmail.com>
2026-07-30 17:52:08 +0530

    Project init and explain basic. Explain useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    // let number = 0;
+  const [number, setNumber] = useState(0);
+  const valueUp = () =>{
+    // number+=1;
+    setNumber(number+1)
+    console.log(number)
+  }
+  const valueDown = () => {
+    // number-=1;
+    setNumber(number - 1)
+    console.log(number)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Number is: {number}</h1>
+      <button onClick={valueUp}>Plus</button>
+      <button onClick={valueDown}>Minus</button>
+    </>
+    
   );
 }
 
