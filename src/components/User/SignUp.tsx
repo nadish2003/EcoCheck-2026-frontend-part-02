580763f7a3a996a55bec16ba3a61f6ae24ad5cdf
Nadish Sathsara <nadishsathsara2003@gmail.com>
2026-08-04 09:12:51 +0530

    Config updated

--- src/components/User/SignUp.tsx
+++ src/components/User/SignUp.tsx
@@ -1,47 +1,106 @@
 export const SignUp = () => {
     return (
-        <>
-            <form>
-                <div className="space-y-12">
-                    <div className="border-b border-white/10 pb-12">
-                        <h2 className="text-base/7 font-semibold text-white">
-                            Profile
-                        </h2>
+         <>
+     
 
-                        <p className="mt-1 text-sm/6 text-gray-400">
-                            This information will be displayed publicly so be careful what you
-                            share.
-                        </p>
+            <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    {/* <img
+                        alt="Your Company"
+                        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                        className="mx-auto h-10 w-auto"
+                    /> */}
+                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-blue-400">Register Now to EcoCheck-2026</h2>
+                </div>
 
-                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <div className="sm:col-span-4">
-                                <label
-                                    htmlFor="username"
-                                    className="block text-sm/6 font-medium text-white"
-                                >
-                                    Username
-                                </label>
-
-                                <div className="mt-2">
-                                    <div className="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-white/10 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-500">
-                                        <div className="shrink-0 text-base text-gray-400 select-none sm:text-sm/6">
-                                            workcation.com/
-                                        </div>
-
-                                        <input
-                                            id="username"
-                                            name="username"
-                                            type="text"
-                                            placeholder="janesmith"
-                                            className="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6"
-                                        />
-                                    </div>
-                                </div>
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6">
+                        <div>
+                            <label htmlFor="email" className="block text-sm/6 font-medium text-blue-400">
+                                Email address
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                    
+                                    required
+                                    className="block w-full rounded-md bg-blue/5 px-3 py-1.5 text-base text-blue outline-1 -outline-offset-1 outline-blue/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                />
                             </div>
                         </div>
-                    </div>
+
+                       <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm/6 font-medium text-blue-400">
+                                    Name
+                                </label>
+                                <div className="text-sm">                               
+                                </div>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="name"
+                                    name="name"
+                                    type="text"
+
+                                    required
+                                    className="block w-full rounded-md bg-blue/5 px-3 py-1.5 text-base text-blue outline-1 -outline-offset-1 outline-blue/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm/6 font-medium text-blue-400">
+                                Password
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+               
+                                    required
+                                    className="block w-full rounded-md bg-blue/5 px-3 py-1.5 text-base text-blue outline-1 -outline-offset-1 outline-blue/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm/6 font-medium text-blue-400">
+                                Role
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="role"
+                                    name="role"
+                                    type="text"
+         
+                                    required
+                                    className="block w-full rounded-md bg-blue/5 px-3 py-1.5 text-base text-blue outline-1 -outline-offset-1 outline-blue/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                />
+                            </div>
+                        </div>
+                    
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+
+                    {/* <p className="mt-10 text-center text-sm/6 text-gray-400">
+                        Not a member?{' '}
+                        <a href="#" className="font-semibold text-blue-400 hover:text-blue-300">
+                            Meet Our Community
+                        </a>
+                    </p> */}
                 </div>
-            </form>
+            </div>
         </>
-    );
+    )
 };
